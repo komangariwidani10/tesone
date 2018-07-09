c2759fdeb604c5678e957452b469beae62aa3b4c
newadmin <newadmin@HPM-1CPU2426>
2018-07-09 05:40:23 -0700

    form3

---
+++ Tes1/Form3.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form3
+    Private Sub Form3_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox("Button 1 from form 1")
+    End Sub
+End Class
